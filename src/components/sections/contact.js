b3ace504daf13cf5ec3a6fc4db8fec988b1cc5d5
nguyen <khacbaoanh.nguyen@gmail.com>
2024-01-04 19:27:59 +0100

    add change for jobs

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -55,17 +55,19 @@ const Contact = () => {
 
   return (
     <StyledContactSection id="contact" ref={revealContainer}>
-      <h2 className="numbered-heading overline">What’s Next?</h2>
+      <h2 className="numbered-heading overline">Prochaine étape ?</h2>
 
-      <h2 className="title">Get In Touch</h2>
+      <h2 className="title">Garde en contact</h2>
 
       <p>
-        Although I’m not currently looking for any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        Je ne suis pas actuellement en recherche une nouvelle opportunité mais je suis toujours prêt
+        d'élargir mes réseaux professionels. Envoyez moi l'invitation sur{' '}
+        <a href="https://www.linkedin.com/in/nguyenkhacbaoanh/">Linkedin</a> ou envoyez par mail
+        ci-dessous.
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
-        Say Hello
+        Envoyez - moi !!!
       </a>
     </StyledContactSection>
   );
